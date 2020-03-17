2b590814e0b8a90d4b76528abcaa368aaa3703bb
wps0 <53907986+wps0@users.noreply.github.com>
2020-03-17 23:04:29 +0100

    Create yt-dl_window.sh

---
+++ yt-dl_window.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#= Constant variables =#
+readonly ROOT_DIR="~"
+readonly OUTPUT_DIR=${ROOT_DIR}"/ytdl-output/"
+# youtube-dl output file's template
+readonly FILE_TEMPLATE="%%(title)s.%%(ext)s"
+readonly PLAYLIST_TEMPLATE="%%(playlist)s/%%(playlist_index)s - %%(title)s.%%(ext)s"
+ytdlArguments="--geo-bypass -x --audio-format \"wav\" --add-metadata"
+
+echo -n "Youtube video/playlist URL: "
+read ytUrl
+
+if [[ "${ytUrl##playlist}" == $ytUrl ]]; then
+    ytdlArguments="${ytdlArguments} -o \"${OUTPUT_DIR}${FILE_TEMPLATE}\""
+    echo "TYPE: VIDEO"
+elif [[ "${ytUrl##playlist}" != $ytUrl ]]; then
+    ytdlArguments="${ytdlArguments} -o \"${OUTPUT_DIR}${PLAYLIST_TEMPLATE}\""
+    echo "TYPE: PLAYLIST"
+fi
+
+echo "Arguments: ${ytdlArguments} ${ytUrl}"
+
+eval "youtube-dl ${ytdlArguments} ${ytUrl}"
