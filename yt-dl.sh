6960c831595717cb35087a4d4cb052577ccf8a2d
wps0 <53907986+wps0@users.noreply.github.com>
2020-03-18 23:03:08 +0100

    Rename yt-dl_window.sh, fix issue
    Rename yt-dl_window.sh to yt-dl.sh, fix template error

--- yt-dl.sh
+++ yt-dl.sh
@@ -4,18 +4,18 @@
 readonly ROOT_DIR="~"
 readonly OUTPUT_DIR=${ROOT_DIR}"/ytdl-output/"
 # youtube-dl output file's template
-readonly FILE_TEMPLATE="%%(title)s.%%(ext)s"
-readonly PLAYLIST_TEMPLATE="%%(playlist)s/%%(playlist_index)s - %%(title)s.%%(ext)s"
+readonly FILE_TEMPLATE="%(title)s.%(ext)s"
+readonly PLAYLIST_TEMPLATE="%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"
 ytdlArguments="--geo-bypass -x --audio-format \"wav\" --add-metadata"
 
 echo -n "Youtube video/playlist URL: "
 read ytUrl
 
-if [[ "${ytUrl##playlist}" == $ytUrl ]]; then
-    ytdlArguments="${ytdlArguments} -o \"${OUTPUT_DIR}${FILE_TEMPLATE}\""
+if [[ ${ytUrl##playlist} == $ytUrl ]]; then
+    ytdlArguments="${ytdlArguments} -o '${OUTPUT_DIR}${FILE_TEMPLATE}'"
     echo "TYPE: VIDEO"
-elif [[ "${ytUrl##playlist}" != $ytUrl ]]; then
-    ytdlArguments="${ytdlArguments} -o \"${OUTPUT_DIR}${PLAYLIST_TEMPLATE}\""
+elif [[ ${ytUrl##playlist} != $ytUrl ]]; then
+    ytdlArguments="${ytdlArguments} -o '${OUTPUT_DIR}${PLAYLIST_TEMPLATE}'"
     echo "TYPE: PLAYLIST"
 fi
 
